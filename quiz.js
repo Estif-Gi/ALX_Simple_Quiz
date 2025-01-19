b2925c2ce3ec987b19c0cd7e6ee5782194cf83a6
Estifanos Girma <estifanosgirma4@gmail.com>
2025-01-20 00:09:41 +0300

    2+2

--- quiz.js
+++ quiz.js
@@ -1,3 +1,26 @@
+
+const correctAnswer = "4";
+
+function checkAnswer() {
+    const userAnswer = document.querySelector('input[name="quiz"]:checked');
+    const feedback = document.getElementById("feedback");
+    
+    if (userAnswer === correctAnswer ) {
+        feedback.textContent = "Please select an answer.";
+        return;
+    }
+    
+    if (userAnswer.value === correctAnswer) {
+        feedback.textContent = "Correct! Well done.";
+        
+    } else {
+        feedback.textContent = "That's incorrect. Try again!";
+    }
+}
+
+const submitButton = document.getElementById("submit-answer");
+submitButton.addEventListener("click", checkAnswer);
+
 // let checkAnswer = document.querySelector('#submit-answer');
 // let feedback = document.querySelector('#feedback');
 // let choose = document.querySelectorAll("input[name='quiz']");
@@ -19,25 +42,3 @@
 // choose.forEach(choose=>{
 //     choose.addEventListener('change' , findSelected);
 // });
-
-const correctAnswer = "4";
-
-function checkAnswer() {
-    const userAnswer = document.querySelector('input[name="quiz"]:checked');
-    const feedback = document.getElementById("feedback");
-
-    if (userAnswer === correctAnswer ) {
-        feedback.textContent = "Please select an answer.";
-        return;
-    }
-
-    if (userAnswer.value === correctAnswer) {
-        feedback.textContent = "Correct! Well done.";
-    
-    } else {
-        feedback.textContent = "That's incorrect. Try again!";
-    }
-}
-
-const submitButton = document.getElementById("submit-answer");
-submitButton.addEventListener("click", checkAnswer);
